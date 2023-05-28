ddcc04038ed050c621f33c20e73102cf511c986e
qhc <onlinecqh99@gmail.com>
2023-05-28 00:14:53 -0400

    refactor

--- ef_core/DBEndpoints.cs
+++ ef_core/DBEndpoints.cs
@@ -4,16 +4,40 @@ public class DBEndpoints : IEndpointsMapper
 {
     public void MapEndpoints(IEndpointRouteBuilder app)
     {
-        app.MapPost(
-            "/icecreams",
-            async (IcecreamDb db, Icecream icecream) =>
+        var postIcecream = async (IcecreamDb db, Icecream icecream) =>
             {
                 await db.Icecreams.AddAsync(icecream);
                 await db.SaveChangesAsync();
                 return Results.Created($"/icecreams/{icecream.Id}", icecream);
-            });
-        app.MapGet(
-            "/icecreams", 
-            async (IcecreamDb db) => await db.Icecreams.ToListAsync());
+            };
+        var getIcecream = async (IcecreamDb db) => await db.Icecreams.ToListAsync();
+        var updateIcecream = async (IcecreamDb db, Icecream updateicecream, int id) =>
+            {
+                var icecream = await db.Icecreams.FindAsync(id);
+                if (icecream is null)
+                {
+                    return Results.NotFound();
+                }
+                icecream.Name = updateicecream.Name;
+                icecream.Description = updateicecream.Description;
+                await db.SaveChangesAsync();
+                return Results.Ok();
+            };
+        var deleteIcecream = async (IcecreamDb db, int id) =>
+            {
+                var icecream = await db.Icecreams.FindAsync(id);
+                if (icecream is null)
+                {
+                    return Results.NotFound();
+                }
+                db.Icecreams.Remove(icecream);
+                await db.SaveChangesAsync();
+                return Results.Ok();
+            };
+
+        app.MapPost("/icecreams", postIcecream);
+        app.MapGet("/icecreams", getIcecream);
+        app.MapPut("/icecreams/{id}", updateIcecream);
+        app.MapDelete("/icecreams/{id}", deleteIcecream);
     }
 }
